9f566e415c00bc0c3d13629183619a0f3f033bb2
gquliyevanq-beep <gquliyevanq@gmail.com>
2026-09-08 17:00:05 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int x = 1;
+int y = 1;
+int main() {
+    cout << "1) " << x << " + " << y << " = " << x + y << endl;
+    cout << "2) " << x << " - " << y << " = " << x - y << endl;
+    cout << "3) " << x << " * " << y << " = " << x * y << endl;
+    cout << "4) " << x << " / " << y << " = " << x / y << endl;
+}                 
